0b497b62acac66595b54d534b4e5fd5034cc16af
Sidharth Poovattum Kundil(UST <248778@ust.com>
2024-03-15 12:20:15 +0530

    create project structure

---
+++ DigrammingTool-App/src/main/java/com/diagrammingtool/app/DigrammingToolAppApplication.java
@@ -0,0 +1,14 @@
+package com.diagrammingtool.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigrammingToolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigrammingToolAppApplication.class, args);
+		System.out.println("hai");
+	}
+
+}
---
+++ DigrammingTool-App/src/test/java/com/diagrammingtool/app/DigrammingToolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagrammingtool.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigrammingToolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
